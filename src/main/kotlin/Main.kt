8fd4ba2d75e71cb52e35c9722119631f708253b2
giscarpi <carpinteiro.gisel@gmail.com>
2021-06-27 14:40:07 -0500

    se agrega movement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,4 +18,55 @@ fun main() {
         println("Ha inciado sesión el usuario user123")}
 
     //Fin Codigo de prueba
+
+
+
+    var income = mutableListOf<Movement>()
+    var expenses = mutableListOf<Movement>()
+
+    var totalAmountIncome = 0f;
+    var totalAmountExpenses = 0f;
+
+
+    expenses.add(Movement("netflix", "pagar cada mes", 200f, "Month"))
+    expenses.add(Movement("agua", "pagar cada mes", 100f, "Day"))
+    income.add(Movement("Quincena", "pagar cada mes", 100f, "Month"))
+
+
+    income.forEach {
+        totalAmountIncome += it.amount;
+    }
+
+    expenses.forEach {
+        totalAmountExpenses += it.amount;
+    }
+
+    income.forEach { it.getInfoMovements() }
+
+    expenses.forEach { it.getInfoMovements() }
+
+
+    println("Tu total de gastos $totalAmountExpenses")
+    println("Tu total de Ingresos $totalAmountIncome")
+
+
+
+    //Filtrar por mes
+    expenses.forEach {
+        if (it.period == "Month") {
+            println(".....Gastos por mes.......")
+            it.getInfoMovements()
+        }
+    }
+
+    //Filtrar por movimiento
+
+    expenses.forEach {
+        if (it.period == "Day") {
+            println(".....Gastos por dia.......")
+            it.getInfoMovements()
+        }
+    }
+
+
 }
--- src/main/kotlin/Movement.kt
+++ src/main/kotlin/Movement.kt
@@ -1,2 +1,17 @@
-class Movement () {
+class Movement(
+    val name: String,
+    val description: String,
+    val amount: Float,
+    val period: String,
+) {
+
+
+    fun getInfoMovements() {
+        println("Nombre: $name")
+        println("Description: $description")
+        println("Amount: $amount")
+        println("Date: $period")
+        println("-------------------------------------------")
+    }
+
 }
