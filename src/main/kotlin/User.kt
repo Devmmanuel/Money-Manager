673c53d9315445f697592ce624acdfee369c7911
CS-Parra <carlos.parra.s@outlook.com>
2021-06-26 19:32:09 -0500

    Actualización

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,2 +1,28 @@
 class User () {
+
+    val userSaved : String = "user123"
+    val passwordSaved : String = "12345"
+
+
+
+
+    //función para loggear usuario
+    fun login(user : String, password : String ) :Boolean {
+        //Función para validar que es un String y no un espacio vacio
+        fun validate(input: String): Boolean {
+            if (input.isEmpty()) {
+                return false
+            }
+            return true
+        }
+
+        val userValidated = validate(user)
+        val passValidated = validate(password)
+        return if (userValidated && passValidated) {
+            user == userSaved && password == passwordSaved
+        } else false
+
+    }
+
+
 }
